4b8d42fee24e34b38d660427980efdcf1ed86072
Minwoo Jung <jmwsoft@gmail.com>
2015-07-16 13:31:15 +0900

    Create i.c

---
+++ i.c
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <memory.h>
+#include <string.h>
+using namespace std;
+char map[51][51];
+int g,s;
+int main() {
+	cin >> g >>s;
+	for(int i=0;i<g;i++)
+		scanf("%s",map[i]);
+	
+	for(int i=0;i<g;i++){
+	for(int x=0;x<s;x++)
+		printf("%c",map[i][x]);
+		cout << endl;
+	}
+	// your code goes here
+	return 0;
+}
